375ecf52f7bf74049b0c0e9486b5c3c0d3fd3a84
Jonas <53447664+jnsAntunes@users.noreply.github.com>
2021-04-22 22:13:26 -0300

    Teste de Rota com Conexão ao MySQL

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
-var router = express.Router();
+var App = express.Router();
+
+var Empresa = getmodule('api/empresa');
+var Historico = getmodule('api/historico');
+var Login = getmodule('api/login');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+App.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+console.log('TESTE DE ROTA')
+App.get('/empresas', function (req, res) {
+  req.getConnection(function (err, connection) {
+    if (err) return res.status(400).json();
+    connection.query('SELECT * FROM Empresa', [], function (err, result) {
+      if (err) return res.status(400).json();
+      console.log('Conexão ao MYSQL realizada');
+      return res.status(200).json(result);
+    });
+  });
+});
+
+module.exports = App;
