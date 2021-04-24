96e823ccdce85e4c0d0d42c79b09f4f9c78850d1
Jonas <53447664+Lebedeu@users.noreply.github.com>
2021-04-24 11:16:34 -0300

    Consulta das tabelas do banco de dados para teste

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,35 @@ App.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-console.log('TESTE DE ROTA')
-App.get('/empresas', function (req, res) {
+// Para teste, realiza uma consulta no BD que retorna a tabela Empresa
+App.get('/teste/empresas', function (req, res) {
   req.getConnection(function (err, connection) {
     if (err) return res.status(400).json();
     connection.query('SELECT * FROM Empresa', [], function (err, result) {
+      if (err) return res.status(400).json();
+      console.log('Conexão ao MYSQL realizada');
+      return res.status(200).json(result);
+    });
+  });
+});
+
+// Para teste, realiza uma consulta no BD que retorna a tabela Login
+App.get('/teste/usuarios', function (req, res) {
+  req.getConnection(function (err, connection) {
+    if (err) return res.status(400).json();
+    connection.query('SELECT * FROM Login', [], function (err, result) {
+      if (err) return res.status(400).json();
+      console.log('Conexão ao MYSQL realizada');
+      return res.status(200).json(result);
+    });
+  });
+});
+
+// Para teste, realiza uma consulta no BD que retorna a tabela Historico
+App.get('/teste/historicos', function (req, res) {
+  req.getConnection(function (err, connection) {
+    if (err) return res.status(400).json();
+    connection.query('SELECT * FROM Historico', [], function (err, result) {
       if (err) return res.status(400).json();
       console.log('Conexão ao MYSQL realizada');
       return res.status(200).json(result);
