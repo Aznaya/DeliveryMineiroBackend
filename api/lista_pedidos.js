7ee482b1eae487ed58ba605e3afae1fc3b99b30b
Jonas Antunes <jonasantunes@gec.inatel.br>
2021-06-26 13:15:46 -0300

    Adicionando requisição na api de lista de pedidos

--- api/lista_pedidos.js
+++ api/lista_pedidos.js
@@ -1,3 +1,15 @@
+exports.list = function (req, res) {
+    req.getConnection(function (err, connection) {
+        if (err) return res.status(400).json();
+        connection.query(
+            'SELECT * FROM lista_pedidos;',
+            [], function (err, result) {
+                if (err) return res.status(400).json();
+
+                return res.status(200).json(result);
+            });
+    });
+}
 
 exports.getById = function (req, res) {
     var Registro = req.params.id;
